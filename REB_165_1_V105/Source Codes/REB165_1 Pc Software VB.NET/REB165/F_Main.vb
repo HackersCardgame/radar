ac74d1496510e110d6c3d2b759c0e71724a305c1
Your Name <you@example.com>
2022-03-07 13:17:04 +0100

    ...

--- REB_165_1_V105/Source Codes/REB165_1 Pc Software VB.NET/REB165/F_Main.vb
+++ REB_165_1_V105/Source Codes/REB165_1 Pc Software VB.NET/REB165/F_Main.vb
@@ -164,7 +164,7 @@ Public Class F_Main
                                     If C_EnableLog.Checked = True Then                                  'Prüfen ob Log an ist
                                         F_Log.T_Log.Text = F_Log.T_Log.Text & My.Computer.Clock.LocalTime & " " & L_Speed.Text & vbCrLf     'Wert dokumentieren
                                         ''MessageBox.Show("You are in the Form.Shown event.")
-                                        Using sw As StreamWriter = File.AppendText("logfile" & Date.Now.ToString("yyyyMMdd") & ".txt")
+                                        Using sw As StreamWriter = File.AppendText("logfile" & Date.Now.ToString("yyyyMM") & ".txt")
                                             sw.WriteLine(My.Computer.Clock.LocalTime & " " & L_Speed.Text & vbCrLf)
 
                                         End Using
